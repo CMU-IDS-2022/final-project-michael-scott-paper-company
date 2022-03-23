9566d951911c2428ee1d36b528e094be89887f74
Eeshwar <eeshwarg13@gmail.com>
2022-03-22 21:31:02 -0400

    Add streamlit file

---
+++ streamlit_app.py
@@ -0,0 +1,16 @@
+from itertools import groupby
+from pydoc import describe
+import streamlit as st
+import pandas as pd
+import altair as alt
+from vega_datasets import data
+import json
+import time
+st.set_page_config(layout="wide")
+
+st.title("Coming soon!")
+
+st.markdown("This project was created by Sayani, Ramya, Eeshwar and Sumanth for the [Interactive Data Science](https://dig.cmu.edu/ids2022) course at [Carnegie Mellon University](https://www.cmu.edu).")
+
+
+
